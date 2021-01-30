08349eaf54c7181f8a985afa60d19702bce5bacd
Cussa Mitre <cussa.mitre@gmail.com>
2021-01-30 19:28:01 +0000

    feat: add logger in the hub when receive message

--- src/AudienceDisplay/Hubs/AudienceDisplayListener.cs
+++ src/AudienceDisplay/Hubs/AudienceDisplayListener.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 
 namespace AudienceDisplay.Hubs
 {
     public class AudienceDisplayListener : Hub
     {
+        private readonly ILogger<AudienceDisplayListener> _logger;
+
+        public AudienceDisplayListener(ILogger<AudienceDisplayListener> logger)
+        {
+            _logger = logger;
+        }
+
         public override Task OnDisconnectedAsync(Exception exception)
         {
             return base.OnDisconnectedAsync(exception);
@@ -13,6 +21,7 @@ namespace AudienceDisplay.Hubs
 
         public async Task SendMessage(string newId, string author, string imageSrc, string message)
         {
+            _logger.LogInformation($"Message receiveid!\nId: {newId}\nAuthor: {author}\nMessage: {message}");
             await Clients.All.SendAsync("ReceiveMessage", newId, author, imageSrc, message);
         }
     }
