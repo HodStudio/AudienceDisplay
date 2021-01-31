b26d31ca7c66357d70b645fc43b654e54fc90707
Cussa Mitre <cussa.mitre@gmail.com>
2021-01-31 17:01:52 +0000

    feat: cleaning files

--- src/Extension/options.js
+++ src/Extension/options.js
@@ -29,7 +29,6 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-    // Use default value color = 'red' and likesColor = true.
     chrome.storage.sync.get({
         serverAddress: 'http://localhost:40000'
     }, function (items) {
